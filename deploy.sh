63048b36d9f576b2b7614de6287e227e9ac1d435
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-07 18:14:25 +0200

    Deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+echo "Pay - Deploy Script"
+
 host='10.8.0.1'
 remotePath='/var/www/pay/'
 user='ung'
 
-filesList[0] = 'LICENSE'
-filesList[1] = 'README'
-filesList[2] = 'deploy.js'
-filesList[3] = 'package.json'
-filesList[4] = 'server.js'
 
-git pull
-scp -r app ${filesList[*]} ung@'$host':'$remotePath'
-ssh '$user'@'$host' npm start
+filesList[0]='LICENSE'
+filesList[1]='README'
+filesList[2]='deploy.js'
+filesList[3]='package.json'
+filesList[4]='server.js'
+
+echo "--- config ---"
+echo "host       : $host"
+echo "remotePath : $remotePath"
+echo "user       : $user"
+
+echo "--- git pull ---"
+#git pull
+echo "--- rsync ---"
+rsync -rv --exclude='config.json' app ${filesList[*]} ung@"$host":"$remotePath"
+echo "--- ssh ---"
+#ssh "$user"@"$host" cd $remotePath && npm start
