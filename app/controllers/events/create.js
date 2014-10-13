637aa99b41edf086a214b9aafbebaee678e7ea68
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-13 20:41:19 +0200

    Fix event creation (moment date parsing, more precise regexp, etc.)

--- app/controllers/events/create.js
+++ app/controllers/events/create.js
@@ -4,17 +4,24 @@
 
 'use strict';
 
-
 var fs   = require('fs');
 var path = require('path');
+var moment = require('moment');
 
 module.exports = function (db) {
     return function (req, res) {
         var newEvent = req.body;
 
+        if (!req.form.isValid) {
+            Error.emit(res, 400, '400 - Bad Request');
+            return;
+        }
+
+        var form = req.form;
+
         // Save image to upload/
-        var base64Regex = /^data:.+\/(.+);base64,(.*)$/;
-        var matches = newEvent.image.match(base64Regex);
+        var base64Regex = /^data:\w+\/(\w+);base64,([a-zA-Z0-9+\/=]+)$/;
+        var matches = form.image.match(base64Regex);
         
         var ext = matches[1];
         var data = matches[2];
@@ -22,7 +29,7 @@ module.exports = function (db) {
         var buffer = new Buffer(data, 'base64');
 
         var opath = path.resolve(process.cwd() + '/app/public/static/img/upload');
-        var oname = newEvent.name.replace(/\W+/ig, '-') + '.' + ext;
+        var oname = form.name.replace(/\W+/ig, '-') + '.' + ext;
         opath = (opath + '/' + oname).toLowerCase();
         fs.writeFile(opath, buffer, function (err) {
             if (err) {
@@ -30,10 +37,11 @@ module.exports = function (db) {
             }
     
             db.Event.create({
-                name: newEvent.name,
+                name: form.name,
                 picture: path.basename(opath),
-                description: newEvent.description,
-                date: new Date(newEvent.date)
+                description: form.description,
+                date: moment(form.date, 'DD/MM/YYYY HH:mm'),
+                maximumTickets: form.maximumTickets
             });
 
             res.end('ok');
