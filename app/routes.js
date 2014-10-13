bf8dfa184d42014cb5795518ef978a19d2d9380e
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-13 20:32:22 +0200

    Add server-side form validators Add some routes

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,52 @@
 
 'use strict';
 
+var validators = require('./controllers/validators');
+
 module.exports = function (router, db, config) {
     var controllers = require('./controllers')(db, config);
 
-    router.get( '/tickets', controllers.tickets.getAll);
-    router.get( '/events',  controllers.events.getAll);
-    router.post('/events',  controllers.events.create);
-    router.post('/authEtu', controllers.etu);
+    // Tickets
+    router.get(
+        '/tickets',
+        controllers.tickets.getAll
+    );
+
+    // Events
+    router.get(
+        '/events',
+        controllers.events.getAll
+    );
+    router.post(
+        '/events',
+        validators.createEvent,
+        controllers.events.create
+    );
+
+    // Event
+    router.get(
+        '/events/:eventId',
+        controllers.events.getOne
+    );
+
+    // Events ticket
+    router.get(
+        '/events/:eventId/tickets',
+        controllers.tickets.getAllFromEvent
+    );
+
+    // Auth etu
+    router.post(
+        '/authEtu',
+        controllers.etu
+    );
+
+    router.param('eventId', function (req, res, next, eventId) {
+        if (Number.isPositiveNumeric(eventId)) {
+            req.body.eventId = eventId;
+            next();
+        } else {
+            Error.emit(res, 400, '400 - Bad Request');
+        }
+    });
 };
