2778d8d9d405c67eab208d6d51ab4dd2da819843
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-13 20:36:44 +0200

    Form validators

---
+++ app/controllers/validators/createEvent.js
@@ -0,0 +1,34 @@
+// Pay - /app/controllers/validators/createEvent.js
+
+// Tickets creator form validatior
+
+'use strict';
+
+var form  = require('express-form');
+var field = form.field;
+
+module.exports = form(
+    field('name')
+        .trim()
+        .required()
+        .is(/^[A-ÿ0-9 ]{5,255}$/),
+
+    field('date')
+        .trim()
+        .required()
+        .is(/^\d{1,2}\/\d{1,2}\/\d{4} [0-2][1-9]?:[0-5]?[0-9]$/),
+
+    field('description')
+        .trim()
+        .required(),
+
+    field('maximumTickets')
+        .trim()
+        .required()
+        .is(/[0-9]{1,4}/),
+
+    field('image')
+        .trim()
+        .required()
+        .is(/^data:\w+\/\w+;base64,[a-zA-Z0-9+\/=]+$/)
+);
---
+++ app/controllers/validators/index.js
@@ -0,0 +1,11 @@
+// Pay - /app/controllers/validators/index.js
+
+// Validators regroupment file
+
+'use strict';
+
+var createEvent = require('./createEvent');
+
+module.exports = {
+    createEvent: createEvent
+};
