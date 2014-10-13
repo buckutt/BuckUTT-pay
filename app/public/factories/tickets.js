4d073c020e89e9a664fd395f7f618ac166f21b88
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-13 20:36:12 +0200

    Ticket resources

---
+++ app/public/factories/tickets.js
@@ -0,0 +1,13 @@
+// Pay - /app/public/factories/tickets.js
+
+// Ticket(s) resource
+
+'use strict';
+
+pay.factory('Ticket', ['$Resource', function ($resource) {
+    return $resource('api/tickets/:id');
+}]);
+
+pay.factory('EventTickets', ['$Resource', function ($resource) {
+    return $resource('api/events/:id/tickets/');
+}]);
