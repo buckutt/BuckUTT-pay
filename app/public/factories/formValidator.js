17031c4e4bee9c53ebe611596f715fce83b8ba9c
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-16 15:31:01 +0200

    Fix form validation with editing form (values already here)

--- app/public/factories/formValidator.js
+++ app/public/factories/formValidator.js
@@ -5,24 +5,31 @@
 'use strict';
 
 pay.factory('FormValidator', [function () {
-    return function (form, imageSelector) {
+    return function (form, imageSelector, imageValidator) {
             // If we end directly the function, all errors may be not thrown
             var formValid = true;
 
             // Image validation
-            var $file = form[imageSelector];
-            var file = $file.files[0];
-            if (!file || (file.type !== 'image/png' && file.type !== 'image/jpeg')) {
-                $($file).parent().parent().next().addClass('ng-invalid');
-                formValid = false;
-            } else {
-                $($file).parent().parent().next().removeClass('ng-invalid').addClass('ng-valid');
+            console.log(typeof imageValidator !== 'undefined');
+            if (typeof imageValidator !== 'undefined') {
+                var $file = form[imageSelector];
+                var file = $file.files[0];
+                if (!file || (file.type !== 'image/png' && file.type !== 'image/jpeg')) {
+                    $($file).parent().parent().next().addClass('ng-invalid');
+                    formValid = false;
+                } else {
+                    $($file).parent().parent().next().removeClass('ng-invalid').addClass('ng-valid');
+                }
             }
 
             // Input validation
-            var isFormValid = $('.ng-pristine, .ng-invalid', form).length === 0;
+            var isFormValid = $('.ng-pristine, .ng-invalid', form).filter(function () {
+                return this.value.length === 0;
+            }).length === 0;
             if (!isFormValid) {
-                var $invalids = $('.ng-pristine, .ng-invalid', form);
+                var $invalids = $('.ng-pristine, .ng-invalid', form).filter(function () {
+                    return this.value.length === 0;
+                });
                 $invalids.removeClass('ng-pristine ng-valid').addClass('ng-invalid');
                 formValid = false;
             }
