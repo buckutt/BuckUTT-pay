91be5c83fa27cbe10c996df5e8c76188378f9985
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-19 21:50:34 +0200

    Logged bar

--- app/public/login/login.js
+++ app/public/login/login.js
@@ -18,59 +18,6 @@ pay.controller('Login', [
         }
 
         /**
-          * Shows the modal related to the ticket lost
-          * @param {object} e - The click event
-          */
-        this.showModal = function (e) {
-            e.preventDefault();
-            $timeout(function () {
-                $('#modalLost').modal();
-            });
-        };
-
-        /**
-          * Authenticates the user via site etu
-          * @param {object} e - The click event
-          */
-        this.authUser = function (e) {
-            e.preventDefault();
-            SiteEtu.auth('11196275875', treatUserData);
-        };
-
-        /**
-          * Shows the username
-          * @param {function} data - The user data
-          */
-        function treatUserData (data) {
-            SiteEtu.etu = data;
-            $scope.etu = window.x = data;
-
-            $('#loginController').addClass('connecting');
-            $('.guest').hide();
-            $('.loading').hide();
-            $('#loginIcon').addClass('iconHide');
-            $('#okayIcon').addClass('iconShow');
-            setTimeout(function () {
-                $('#okayIcon').removeClass('iconShow');
-                $('#loginController').removeClass('connecting');
-                setTimeout(function () {
-                    $('#logged').removeClass('loggedHide');
-                });
-            }, 1000);
-
-            $('.connected').show();
-
-            /* if (data.isAssoAdmin) */
-            if (1) {
-                $('.admin').show();
-            } else {
-                $('.admin').hide();
-            }
-
-            $('.welcoming').text('Bonjour, ' + data.fullName).show();
-        }
-
-        /**
           * Shows the loading icons
           */
         function treatLoading () {
@@ -107,5 +54,66 @@ pay.controller('Login', [
                 duration: 300
             }, 'easeInOutExpo');
         }
+
+        /**
+          * Shows the username
+          * @param {function} data - The user data
+          */
+        function treatUserData (data) {
+            SiteEtu.etu = data;
+            $scope.etu = window.x = data;
+
+            $('#loginController').addClass('connecting');
+            $('#loginIcon').addClass('iconHide');
+            $('#okayIcon').addClass('iconShow');
+            $('#buttonLost').fadeOut();
+            setTimeout(function () {
+                $('#okayIcon').removeClass('iconShow');
+                $('#loginController').removeClass('connecting');
+                $('#logged').removeClass('loggedHide');
+                $('.loginPanel').addClass('logged');
+            }, 1000);
+
+            $('.connected').show();
+
+            /* if (data.isAssoAdmin) */
+            if (1) {
+                $('.admin').show();
+            } else {
+                $('.admin').hide();
+            }
+
+            $('.welcoming').text('Bonjour, ' + data.fullName).show();
+        }
+
+        /**
+          * Shows the modal related to the ticket lost
+          * @param {object} e - The click event
+          */
+        this.showModal = function (e) {
+            e.preventDefault();
+            $timeout(function () {
+                $('#modalLost').modal();
+            });
+        };
+
+        /**
+          * Authenticates the user via site etu
+          * @param {object} e - The click event
+          */
+        this.authUser = function (e) {
+            e.preventDefault();
+            SiteEtu.auth('11196275875', treatUserData);
+        };
+
+        /**
+          * Logs out the user
+          * @param {object} e - The click event
+          */
+        this.logout = function (e) {
+            e.preventDefault();
+            localStorage.clear();
+            window.location.reload();
+        };
     }
 ]);
