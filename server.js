415ccc74a2a20db218b86556aaaae05067efbe1d
Gabriel Juchault <gabriel.juchault@gmail.com>
2014-10-27 20:03:04 +0100

    New error messages

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ Date.prototype.toDateTime = function () {
 // DRY error module
 Error.emit = function (res, status, msg, additionnalLog) {
     var msgCodes = {
-        '500 - Etu server is not responding': 1,
-        '400 - Bad Request'                 : 2,
-        '500 - SQL Server error'            : 3,
-        '400 - Not enough scopes'           : 4,
-        '400 - Duplicate event'             : 7,
-        '500 - Cannot write file'           : 8,
-        '500 - Invalid token'               : 10
+        '404 - Not Found'                 : 1,
+        '400 - Bad Request'               : 2,
+        '500 - SQL Server error'          : 3,
+        '400 - Invalid username/password' : 4,
+        '400 - Duplicate event'           : 7,
+        '500 - Cannot write file'         : 8,
+        '500 - Invalid token'             : 10
     };
 
     log.error(msg);
