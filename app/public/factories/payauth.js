8d017ac41d7ecf4d81f3b6384a603195bed50bc2
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-01-20 02:55:12 +0100

    Event on auth

--- app/public/factories/payauth.js
+++ app/public/factories/payauth.js
@@ -4,65 +4,72 @@
 
 'use strict';
 
-pay.factory('PayAuth', ['$http', '$q', 'Error', function ($http, $q, Error) {
-    var tokenData = null;
-    var etuData = null;
+pay.factory('PayAuth', [
+    '$http',
+    '$q',
+    '$rootScope',
+    'Error',
+    function ($http, $q, $rootScope, Error) {
+        var tokenData = null;
+        var etuData = null;
 
-    function PayAuth () {
-        this.token = tokenData;
-        this.etu = etuData;
-        var self = this;
-        pay.auth = self;
+        function PayAuth () {
+            this.token = tokenData;
+            this.etu = etuData;
+            var self = this;
+            pay.auth = self;
 
-        /**
-          * Authenticates the user via username and password
-          * @param {string} username - The username
-          * @param {string} password - The password
-          */
-        this.auth = function (username, password) {
-            return $q(function (resolve, reject) {
-                $http.post('api/etu/auth', {
-                    username: username,
-                    password: password
-                }).success(function (data) {
-                    self.token = data.token;
-                    self.etu = data;
-                    resolve(data);
-                }).error(function (data, status, headers) {
-                    if (!data) {
-                        Error('Erreur', 15);
-                        return;
-                    }
+            /**
+              * Authenticates the user via username and password
+              * @param {string} username - The username
+              * @param {string} password - The password
+              */
+            this.auth = function (username, password) {
+                return $q(function (resolve, reject) {
+                    $http.post('api/etu/auth', {
+                        username: username,
+                        password: password
+                    }).success(function (data) {
+                        self.token = data.token;
+                        self.etu = data;
+                        $rootScope.$emit('payauth:logged');
+                        resolve(data);
+                    }).error(function (data, status, headers) {
+                        if (!data) {
+                            Error('Erreur', 15);
+                            return;
+                        }
 
-                    // Custom handle wrong auth
-                    if (data && data.error === 4) {
-                        reject(true);
-                        return;
-                    }
-                    Error('Erreur', data.error);
-                    reject(false);
+                        // Custom handle wrong auth
+                        if (data && data.error === 4) {
+                            reject(true);
+                            return;
+                        }
+                        Error('Erreur', data.error);
+                        reject(false);
+                    });
                 });
-            });
-        };
+            };
 
-        /**
-          * Makes the controller require auth
-          */
-        this.needUser = function () {
-            if (!this.token) {
-                location.hash = '#/';
-            }
-        };
+            /**
+              * Makes the controller require auth
+              */
+            this.needUser = function () {
+                if (!this.token) {
+                    location.hash = '#/';
+                }
+            };
 
-        /**
-          * Makes the controller require admin rights
-          */
-        this.needAdmin = function () {
-            if (!this.etu.admin) {
-                location.hash = '#/';
-            }
-        };
+            /**
+              * Makes the controller require admin rights
+              */
+            this.needAdmin = function () {
+                if (!this.etu.admin) {
+                    location.hash = '#/';
+                }
+            };
+        }
+
+        return new PayAuth();
     }
-
-    return new PayAuth();
-}]);
+]);
