92394f985d4085f0442b5ea3c845fe61d6f1d237
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-01-26 19:23:50 +0100

    Search user is okay (add user is not)

--- app/controllers/etu/searchUsers.js
+++ app/controllers/etu/searchUsers.js
@@ -5,28 +5,51 @@
 'use strict';
 
 var request = require('request');
+var _       = require('lodash');
 
 module.exports = function (db, config) {
     var logger = require('../../lib/log')(config);
+    var rest   = require('../../lib/rest')(config, logger);
 
     return function (req, res) {
-        res.json([
-            {
-                fullName: 'Thomas Chauchefoin',
-                id: 1
-            },
-            {
-                fullName: 'Julien Canat',
-                id: 2
-            },
-            {
-                fullName: 'Gabriel Juchault',
-                id: 3
-            },
-            {
-                fullName: 'Hugo Collignon',
-                id: 4
+        rest.get('users/?lastname=%' + req.query.query + '%').success(function (data) {
+            var responseData;
+
+            if (!data) {
+                res.json([]);
+                return;
             }
-        ]);
+
+            if (require('util').isArray(data)) {
+                if (data.length === 0) {
+                    res.json([]);
+                    return;
+                }
+
+                responseData = data.map(function (user) {
+                    return {
+                        id: user.id,
+                        fullName: user.firstname.nameCapitalize() + ' ' + user.lastname.nameCapitalize()
+                    };
+                });
+            } else {
+                if (!data.hasOwnProperty('id')) {
+                    res.json([]);
+                    return;
+                }
+
+                responseData = [
+                    {
+                        id: data.id,
+                        fullName: data.firstname.nameCapitalize() + ' ' + data.lastname.nameCapitalize()
+                    }
+                ];
+            }
+
+            res.json(_.uniq(responseData, 'id'));
+        }).error(function () {
+            Error.emit(res, 500, '500 - Buckutt server error', 'Search failed');
+            return;
+        });
     }
 };
