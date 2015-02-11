32b4637145a50196fc3b72800d1325f73aa73607
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-02-11 21:05:10 +0100

    Default controller

---
+++ app/controllers/etu/resetPwd.js
@@ -0,0 +1,13 @@
+///////////////////////////////
+// Changes the user password //
+///////////////////////////////
+
+'use strict';
+
+module.exports = function (db, config) {
+    var logger = require('../../lib/log')(config);
+    var rest   = require('../../lib/rest')(config, logger);
+
+    return function (req, res) {
+    };
+};
