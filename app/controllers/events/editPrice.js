0f7e43be00ebbffbff17d79b4b9b91396ea57566
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-02-19 17:16:49 +0100

    Starting editing prices

--- app/controllers/events/editPrice.js
+++ app/controllers/events/editPrice.js
@@ -8,6 +8,7 @@ var moment = require('moment');
 
 module.exports = function (db, config) {
     var logger = require('../../lib/log')(config);
+    var rest   = require('../../lib/rest')(config, logger);
 
     return function (req, res) {
         if (!req.form.isValid) {
@@ -18,11 +19,14 @@ module.exports = function (db, config) {
             if (err) {
                 return Error.emit(res, 500, '500 - SQL Server error', err.toString());
             }
+
+            var oldPrice = req.form.price * 10;
             price.price = req.form.price;
             price.save().complete(function (saveErr) {
                 if (saveErr) {
                     return Error.emit(res, 500, '500 - SQL Server error', saveErr.toString());
                 }
+
                 res.json(req.form);
             });
         });
