bef3cf1dabe0c6147ab170bce44a00548c1eb0c4
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-02-23 17:03:27 +0100

    Now remove an event

--- app/controllers/events/remove.js
+++ app/controllers/events/remove.js
@@ -4,21 +4,55 @@
 
 'use strict';
 
-module.exports = function (db) {
+var Promise = require('bluebird');
+var fs      = require('fs');
+
+module.exports = function (db, config) {
+    var logger = require('../../lib/log')(config);
+    var rest   = require('../../lib/rest')(config, logger);
+
     return function (req, res) {
         db.Event.find(req.params.eventId).complete(function (err, event) {
             if (err) {
                 return Error.emit(res, 500, '500 - SQL Error', err.toString());
             }
 
-            event.destroy().complete(function (errDestroy) {
-                if (errDestroy) {
-                    return Error.emit(res, 500, '500 - SQL Error', errDestroy.toString());
-                }
+            var prices;
 
-                res.json({
-                    status: 200
+            // Get prices to remove periods
+            rest.get('prices?ArticleId=' + event.backendId).then(function (rPrices) {
+                prices = rPrices.data.data;
+                var periodId = prices[0].PeriodId;
+
+                // Remove period
+                return rest.delete('periods/' + periodId);
+            }).then(function () {
+                // Remove prices
+                prices = prices.map(function (price) {
+                    return rest.delete('prices/' + price.id);
                 });
+
+                return Promise.all(prices);
+            }).then(function () {
+                // Remove Article (should remove ArticlePoints too)
+                return rest.delete('articles/' + event.backendId);
+            }).then(function () {
+                var picturePath = path.resolve(process.cwd() + '/app/public/static/img/upload') + '/' + event.picture;
+                var newPicturePath = picturePath + '.deleted';
+                fs.rename(picturePath, newPicturePath);
+
+                event.destroy().complete(function (errDestroy) {
+                    if (errDestroy) {
+                        return Error.emit(res, 500, '500 - SQL Error', errDestroy.toString());
+                    }
+
+                    res.json({
+                        status: 200
+                    });
+                });
+            }).catch(function (err) {
+                console.dir(err);
+                Error.emit(res, 500, '500 - Buckutt server error', 'Remove prices');
             });
         });
     };
