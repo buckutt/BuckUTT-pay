ad2fb2feda62db45fe33831febd27a91baf49740
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-03-10 17:22:42 +0100

    Preparing card payment

---
+++ app/controllers/validators/buyTicketCard.js
@@ -0,0 +1,15 @@
+/////////////////////////////////////////
+// Buy ticket with card form validator //
+/////////////////////////////////////////
+
+'use strict';
+
+var form  = require('express-form');
+var field = form.field;
+
+module.exports = form(
+    field('birthdate')
+        .trim()
+        .required()
+        .is(/^\d{1,2}\/\d{1,2}\/\d{4}$/)
+);
